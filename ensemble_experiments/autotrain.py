326f072286e51654f9574091d52bc1f983aed325
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2018-04-11 00:44:40 +1000

    Saving data & nets, Improve Training

--- ensemble_experiments/autotrain.py
+++ ensemble_experiments/autotrain.py
@@ -2,11 +2,58 @@
 Given a dataset, and specs for input and output of a neural network, this module will attempt to
 autotrain to optimization
 """
+import time
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import pandas as pd
 import numpy as np
+
+def epoch_optimise(original_model, max_epochs, train_data, train_classes, test_data, test_classes, compile_args, *, verbose=True, lr=0.1, return_optimum=False):
+
+    start_time = time.time()
+    model = keras.models.clone_model(original_model) # Avoid mutating original
+    model.compile(**compile_args)
+    model.optimizer.lr = lr
+    compile_time = time.time()
+
+    batch_size = len(train_data) # Data all fits in memory (I hope)
+
+    hist = model.fit(
+        train_data,
+        train_classes,
+        verbose=0,
+        epochs=max_epochs,
+        initial_epoch=0,
+        validation_data=(test_data, test_classes),
+        batch_size=batch_size
+    )
+    fit_time = time.time()
+    test_accuracies = hist.history["val_acc"]
+    index = np.argmax(test_accuracies)
+    accuracy = test_accuracies[index]
+    accuracy_time = time.time()
+    if verbose:
+        print(f"      Accuracy {accuracy * 100:.4f}% at Epoch {index}\n"
+              f"      C: {compile_time - start_time}s, F: {fit_time - compile_time}, A: {accuracy_time - fit_time}s")
+
+    if return_optimum:
+        # Train model to optimum epochs
+        max_accuracy_model = keras.models.clone_model(original_model) # Avoid mutating original
+        model.compile(**compile_args)
+        model.optimizer.lr = lr
+        hist = model.fit(
+            train_data,
+            train_classes,
+            verbose=0,
+            epochs=index,
+            initial_epoch=0,
+            batch_size=batch_size
+        )
+    else:
+        max_accuracy_model = None
+
+    return max_accuracy_model, index, accuracy
 
 
 def autotrain(data: pd.DataFrame,
@@ -14,9 +61,9 @@ def autotrain(data: pd.DataFrame,
               *,
               input_cols=("x", "y"),
               class_col="class",
-              min_nodes=2,
+              min_nodes=1,
               max_nodes=9,
-              epoch_step=1,
+              epoch_max=3000,
               verbose=False
     ) -> Sequential:
     """
@@ -35,7 +82,8 @@ def autotrain(data: pd.DataFrame,
         The trained model, and a copy of the model overtrained by 2000 epochs
         if also_overtrain is True
     """
-    assert epoch_step > 0
+    start_time = time.time()
+    assert epoch_max > 0
     assert max_nodes >= min_nodes
 
     inputs = len(input_cols)
@@ -44,87 +92,62 @@ def autotrain(data: pd.DataFrame,
     train_data = data.as_matrix(columns=input_cols)
     test_data = data.as_matrix(columns=input_cols)
 
-    loss_function = None
     if outputs == 2:
         outputs = 1
         loss_function = 'binary_crossentropy'
         train_classes = data["class"].values
         test_classes = data["class"].values
+        optimizer = 'rmsprop'
     else:
         loss_function = 'categorical_crossentropy'
         train_classes = keras.utils.to_categorical(data["class"].values, num_classes=outputs)
         test_classes = keras.utils.to_categorical(data["class"].values, num_classes=outputs)
+        optimizer = 'sgd'
 
-    max_accuracy_so_far = 0
-    max_accuracy_model = None
-
+    compile_args = dict(
+        loss=loss_function,
+        optimizer=optimizer,
+        metrics=['accuracy']
+    )
+    # Determine the best number of hidden nodes to use
+    hidden_nodes_max_accuracy = 0
+    best_num_hidden_nodes = None
     for hidden_nodes in range(min_nodes, max_nodes+1):
         if verbose:
-            print("Train For Hidden Nodes:", hidden_nodes)
+            print("    Train For Hidden Nodes:", hidden_nodes)
         model = Sequential([
             Dense(hidden_nodes, input_shape=(inputs,), activation='sigmoid'),
             Dense(outputs, activation='sigmoid')
         ])
-
-        model.compile(
-            # TODO: Switch to binary_crossentropy if outputs are 2 and use a single output
-            loss=loss_function,
-            optimizer='sgd',
-            metrics=['accuracy']
-        )
-
-        for learning_rate in np.arange(0.01, 0.5, 0.02):
-            model.optimizer.lr = learning_rate
-            if verbose:
-                print(f"  With Learning Rate: {model.optimizer.lr:.4f}")
-
-            trained_to_optimal_epoch = False
-            next_epoch = epoch_step
-            max_accuracy_with_this_learning_rate = 0
-            epoch_of_max_accuracy_with_this_learning_rate = 0
-            while not trained_to_optimal_epoch:
-                hist = model.fit(
-                    train_data,
-                    train_classes,
-                    verbose=0,
-                    epochs=next_epoch,
-                    initial_epoch=next_epoch-1
-                )
-                train_accuracy = hist.history["acc"][-1]
-                loss, accuracy = model.evaluate(
-                    test_data,
-                    test_classes,
-                    verbose=0,
-                )
-                # Test if there has been an accuracy improvement
-                if accuracy > max_accuracy_with_this_learning_rate:
-                    max_accuracy_with_this_learning_rate = accuracy
-                    epoch_of_max_accuracy_with_this_learning_rate = next_epoch
-
-                # Early exit conditions
-                # if the last peak accuracy was more than 2000 steps ago,
-                # or the last peak was within 45% of the start of the run,
-                # and more than 300 epochs have elapsed
-                if (((epoch_of_max_accuracy_with_this_learning_rate + 2000) < next_epoch
-                     or
-                     epoch_of_max_accuracy_with_this_learning_rate < (next_epoch * 0.45)
-                    ) and next_epoch > 300):
-                    if verbose:
-                        print(f"    Found optimal epoch: {next_epoch}.\n"
-                          f"      Params: nodes: {hidden_nodes}, lr: {learning_rate:.4f}, "
-                          f"train accuracy: {train_accuracy:.4f} test accuracy: {accuracy:.4f}")
-                    trained_to_optimal_epoch = True
-                    if accuracy > max_accuracy_so_far:
-                        max_accuracy_so_far = accuracy
-                        max_accuracy_model = keras.models.clone_model(model)
-                        max_accuracy_model.set_weights(model.get_weights())
-
-                next_epoch += epoch_step
-
-    # Todo - show some info about the final model
+        _, _, accuracy = epoch_optimise(model, epoch_max, train_data, train_classes, test_data, test_classes, compile_args, verbose=verbose)
+        if accuracy > hidden_nodes_max_accuracy:
+            best_num_hidden_nodes = hidden_nodes
     if verbose:
-        print(f"Testing Accuracy of trained Model: {max_accuracy_so_far:.4f}")
+        print(f"Found best hidden nodes {best_num_hidden_nodes}")
 
-    # print("": max_accuracy_model)
-    # TODO: Return a saved file rather than
-    return max_accuracy_model
+    model = Sequential([
+        Dense(best_num_hidden_nodes, input_shape=(inputs,), activation='sigmoid'),
+        Dense(outputs, activation='sigmoid')
+    ])
+
+    best_lr = None
+    best_epochs = None
+    best_accuracy = 0
+    best_model = None
+    for lr in np.concatenate([np.arange(0.1, 0.4, 0.05), np.arange(0.09, 0.04, -0.01)]):
+        if verbose:
+            print(f"    Testing LR: {lr}")
+        returned_model, epochs, accuracy = epoch_optimise(model, epoch_max, train_data, train_classes, test_data, test_classes, compile_args, verbose=verbose, lr=lr, return_optimum=True)
+        if accuracy > best_accuracy:
+            best_epochs = epochs
+            best_accuracy = accuracy
+            best_lr = lr
+            best_model = returned_model
+
+    end_time = time.time()
+    if verbose:
+        print(f"    Epoch: {best_epochs}.\n Nodes: {best_num_hidden_nodes},"
+              f"    LR: {best_lr:.4f}, Acc: {best_accuracy:.4f}\n"
+              f"    Time {end_time - start_time}s")
+
+    return best_model
--- ensemble_experiments/experiment2d.py
+++ ensemble_experiments/experiment2d.py
@@ -1,48 +1,94 @@
 """
 Glue code for conducting experiment 1
 """
+import os
+# os.environ["TF_MIN_GPU_MULTIPROCESSOR_COUNT"] = "4"
+# No verbose logging from TF
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+
+
 import argparse
+import signal
 import numpy as np
+from pathlib import Path
+import keras
+import pandas
+
+
+def train(train, test, save_dir, epoch_max, verbose):
+    save_net = save_dir / "net.h5"
+    if not save_net.exists():
+        import ensemble_experiments.autotrain as at
+        net = at.autotrain(train, test, min_nodes=1, max_nodes=9, epoch_max=epoch_max, verbose=verbose)
+        print(f"    SAVING {save_net}")
+        net.save(save_net)
+        return save_net
+    else:
+        print(f"    EXISTING {save_net}")
+        return save_net
 
 def main(args):
     print("Running Experiment")
-    np.random.seed(args.seed)
+    np.random.seed(2000)
     import ensemble_experiments.datagen2d as dg
 
-    for error_rate in range(10, 50, 10):
-        print(f"BEGIN RUN FOR ERROR RATE {error_rate}%")
+    exdir = Path(f"{args.prefix}experiment-EPOCH_MAX={args.epoch_max}-DATA_SIZE={args.data_size}")
+    exdir.mkdir(exist_ok=True)
 
-        # generate data and split into test/train/validate
-        data = dg.generate_data(300, error_rate, 2017)
-        val_data = dg.generate_data(1000, error_rate, 2018)
+    print(f"BEGIN RUN FOR ERROR RATE {args.error_rate}%")
+    ratedir = exdir / f"error-{args.error_rate}"
+    ratedir.mkdir(exist_ok=True)
 
-        train_data = data[::2]
-        test_data = data[1::2]
+    data = dg.generate_data(args.data_size, args.error_rate, 2017)
+    train_data = data[::2]
+    test_data = data[1::2]
 
-        for num_component_nets in range(1, 99):
-            print(f" BEGIN RUN FOR {num_component_nets} NETS")
-            train_test_data = []
-            # Generate num_component_nets bags of data and train networks
-            for i in range(num_component_nets):
+    val_data = dg.generate_data(1000, args.error_rate, 2018)
+
+    train_data.to_csv(ratedir / "initial_train.csv")
+    test_data.to_csv(ratedir / "initial_test.csv")
+    val_data.to_csv(ratedir / "validation.csv")
+
+    for num_component_nets in range(1, args.num_nets+1):
+        print(f" BEGIN RUN FOR {num_component_nets} NETS")
+        working_dir = ratedir / f"ANNE-{num_component_nets}"
+        working_dir.mkdir(exist_ok=True)
+        train_test_data = []
+        # Generate num_component_nets bags of data and train networks
+        for i in range(num_component_nets):
+            save_dir = working_dir / f"net-{i}"
+            save_train = save_dir / "train.csv"
+            save_test = save_dir / "test.csv"
+            save_dir.mkdir(exist_ok=True)
+            if not save_train.exists() and not save_test.exists():
                 train_bag = train_data.sample(len(train_data), replace=True)
                 test_bag = test_data.sample(len(test_data), replace=True)
-                train_test_data.append((
-                    train_bag, test_bag
-                ))
+            else:
+                train_bag = pandas.read_csv(save_train)
+                test_bag = pandas.read_csv(save_test)
 
-            def train(x):
-                import ensemble_experiments.autotrain as at
-                return at.autotrain(*x, min_nodes=2, max_nodes=2, epoch_step=args.step, verbose=args.verbose)
+            train_test_data.append((
+                train_bag, test_bag, save_dir
+            ))
+            train_bag.to_csv(save_train)
+            test_bag.to_csv(save_test)
 
-            networks = list(map(train, train_test_data)) # TODO: Create a dask graph here, we can distribute the training
+        from multiprocessing.pool import Pool
 
-            # TODO: Run validation across component networks and aggregate results into predictions, compare with validation (real) classes
-            print(networks)
+        def wrap_train(x):
+            return train(*x)
 
-        # network = at.autotrain(data, test_data, min_nodes=2, max_nodes=9, epoch_step=args.step, verbose=args.verbose)
+        arguments = [(*x, args.epoch_max, args.verbose) for x in train_test_data]
+        networks = list(map(wrap_train, arguments))
+
+        # TODO: Run validation across component networks and aggregate results into predictions, compare with validation (real) classes
+        print(f"Nets for ANNE {num_component_nets}: {networks}")
 
 def setup_parser(parser: argparse.ArgumentParser):
     parser.add_argument("--verbose", action='store_true', help="Show training logs verbosely")
-    parser.add_argument("--step", type=int, help="Number of epochs between evaluations", default=1)
-    parser.add_argument("--seed", type=int, help="Numpy randon seed", default=2018)
+    parser.add_argument("--epoch-max", type=int, help="Maximum epochs to test to", default=1500)
+    parser.add_argument("--error-rate", type=int, help="Error rate to use", default=10)
+    parser.add_argument("--data-size", type=int, help="Size of train/test dataset", default=300)
+    parser.add_argument("--prefix", help="Prefix for the experiment folder", default="")
+    parser.add_argument("--num-nets", type=int, help="Number of component nets to do the experiment to", default=99)
     parser.set_defaults(func=main)
